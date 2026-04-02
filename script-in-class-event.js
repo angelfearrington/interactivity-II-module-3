c36f6fcab7bdf63c14d25d04f5fd553d036c7bbf
Angelfearrington <anfearringt@uncg.edu>
2026-04-02 09:30:10 -0400

    Followed Kaelyn's Event Listener Tutorial Video

--- script-in-class-event.js
+++ script-in-class-event.js
@@ -1,6 +1,15 @@
-const button = document.getElementById('thisButton');
+const button1 = document.getElementById('thisButton');
 
-button.addEventListener('click', function(){
+button1.addEventListener("click", thisButton);
+
+function thisButton(){
+    alert("Hello Guys!");
+};
+
+
+const button2 = document.getElementById('clickThisButton');
+
+button2.addEventListener('click', function(){
     document.getElementById('message').innerText = "Button Was Clicked!";
 });
 
