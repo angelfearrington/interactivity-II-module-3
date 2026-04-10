906a28a2f8c75709d0d3067406cd0420860a1c41
Angelfearrington <anfearringt@uncg.edu>
2026-04-10 14:51:15 -0400

    WIP1 - Setting up basic layout and functions for my project 3 idea

--- scriptWIP1.js
+++ scriptWIP1.js
@@ -1,6 +1,8 @@
+//------------WRONG BUTTON EVENTS--------------//
+//button1
 function buttonPopUp(){
     var txt;
-    if(confirm("Is this the correct button? Click 'Cancel' if Fasle. Click 'OK' if True" )){
+    if(confirm("Is this the correct button? Click 'Cancel' if False. Click 'OK' if True" )){
         txt = "Sorry, this is NOT the correct button! Try Again!!";
     }else{
         txt = "You're right, this is NOT the correct button! Try Again!";
@@ -8,6 +10,47 @@ function buttonPopUp(){
     document.getElementById("popUp").innerHTML = txt;
 }
 
+//button2
 function wrongButton(){
     alert('WRONG!');
+}
+
+//button3
+const myButton = document.getElementById("button3");
+myButton.addEventListener('mouseenter',()=>{
+    myButton.textContent = 'DO NOT CLICK ME!';
+    }); 
+  myButton.addEventListener('mouseleave',()=>{
+    myButton.textContent = 'CLICK Me!';
+});
+function dontClick(){
+    alert('Why did you click the button? This is the WRONG button. I tried to warn you!');
+}    
+
+//button5
+function changeText(id){
+    id.innerHTML = "INCORRECT!";
+}
+
+//button6
+const buttonSix = document.getElementById('button6');
+let clickCount = 0;
+buttonSix.addEventListener('click', function(){
+    clickCount++;
+    if(clickCount === 1){
+        this.innerText = "If this button is INCORRECT, it will DISAPPEAR when clicked again."
+    } else if(clickCount === 2){
+        this.style.display ='none';
+    }
+});
+
+
+
+//------------CORRECT BUTTON EVENT--------------//
+//button4
+function correctButton(){
+    alert('CORRECT!!!')
+}
+function textChange(id){
+    id.innerHTML = "CORRECT!! CONGRATS!!!"
 }
